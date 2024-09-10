d3bbc026f6b46842aca4670e000719b98b5390cb
hynseok <wefwef12e@naver.com>
2024-09-10 11:44:21 +0900

    feat: Footer 컴포넌트 추가

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./Footer.module.css";
+import { FOOTER_INFO } from "./constants";
+
+export function Footer() {
+  return (
+    <footer className={classes.root}>
+      <div className={classes.wrapper}>
+        <Link href={"https://www.kiise.or.kr"} target="_blank">
+          <Image className={classes.image} src={"/kiise.svg"} alt={""} width={200} height={100} />
+        </Link>
+        <div className={classes.info}>
+          {FOOTER_INFO.map((info, index) => (
+            <div key={index}>
+              {Object.keys(info)[0] === ""
+                ? `${Object.values(info)[0]}`
+                : `${Object.keys(info)[0]} : ${Object.values(info)[0]}`}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={classes.copy}>Copyright &copy; 2024 KIISE All rights reserved. </div>
+    </footer>
+  );
+}
