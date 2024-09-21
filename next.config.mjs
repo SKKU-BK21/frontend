2aecba98c14f4b0850c06179d9733a6489bd9840
hynseok <wefwef12e@naver.com>
2024-09-21 16:45:22 +0900

    feat: MSW(mock api) 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '_http_common'];
+      config.target = 'node';
+    }
+
+    return config;
+  }
+};
 
 export default nextConfig;
