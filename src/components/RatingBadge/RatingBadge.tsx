72e4c6e6e6fe7695c276e7b2bc1e59184486d0f2
hynseok <wefwef12e@naver.com>
2024-09-21 16:51:55 +0900

    fix: 필드명에 따라 grade를 rating으로 수정

---
+++ src/components/RatingBadge/RatingBadge.tsx
@@ -0,0 +1,32 @@
+import classes from "./RatingBadge.module.css";
+
+type Rating = "top" | "excellence";
+
+interface IRatingBadgeProps {
+  year: number;
+  rating: Rating;
+  width?: string;
+  height?: string;
+  backgroundColor?: string;
+  color?: string;
+}
+
+export function RatingBadge({
+  year,
+  rating,
+  width,
+  height,
+  backgroundColor,
+  color,
+}: IRatingBadgeProps) {
+  return (
+    <div
+      className={`${classes.container} ${rating === "top" ? classes.red : classes.blue}`}
+      style={{ width: width, height: height, backgroundColor: backgroundColor }}
+    >
+      <div className={classes.element} style={{ color: color }}>
+        {`${year} ${rating === "top" ? "최우수" : "우수"}`}
+      </div>
+    </div>
+  );
+}
